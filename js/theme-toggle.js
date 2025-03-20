87729d5aa6bb81993c5f504480030aaf462345fc
Gideon Mrina <gidepan002@gmail.com>
2025-03-20 12:02:51 +0300

    Create project page and still bugs on dark theme

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,25 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const themeToggleBtn = document.getElementById('theme-toggle');
-    const imageElement = document.getElementById('toggle-image');
     const themeIcon = themeToggleBtn.querySelector('img');
     const body = document.body;
     const navbar = document.querySelector('.navbar');
     const footer = document.querySelector('.footer');
 
-    const images = [
-        'asset/bulb-on.png',
-        'asset/bulb-off.png',
-    ];
+    // Apply theme from local storage
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-theme');
+        navbar.classList.add('dark-theme');
+        footer.classList.add('dark-theme');
+        themeIcon.src = 'asset/bulb-off.png';
+    }
 
-    let currentIndex = 0;
-
-    themeToggleBtn.addEventListener('click', () => {
-        // Update the index
-        currentIndex = (currentIndex + 1) % images.length;
-        
-        // Change the image source
-        imageElement.src = images[currentIndex];
-    });
     themeToggleBtn.addEventListener('click', function (e) {
         e.preventDefault(); // prevent anchor navigation
         body.classList.toggle('dark-theme');
@@ -29,9 +22,11 @@ document.addEventListener('DOMContentLoaded', function () {
         if (body.classList.contains('dark-theme')) {
             // Change icon to bulb-off when in dark mode
             themeIcon.src = 'asset/bulb-off.png';
+            localStorage.setItem('theme', 'dark');
         } else {
             // Change icon to bulb-on when in light mode
             themeIcon.src = 'asset/bulb-on.png';
+            localStorage.setItem('theme', 'light');
         }
     });
 });
